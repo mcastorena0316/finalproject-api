5d9c8e62d12ab68cdca663b2ac237ac612e5c26b
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-12 19:14:51 -0500

    changes to session controller to fix mistakes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Helpers
 
   skip_before_action :verify_authenticity_token, raise: false
-  
+
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
   def login!
     session[:user_id] = @user.id
