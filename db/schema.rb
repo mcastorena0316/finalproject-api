7f28948151282a43f96d293cad7a2d7041473824
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-18 20:15:20 -0500

    change format for mood, modify controller for trackings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_212525) do
+ActiveRecord::Schema.define(version: 2020_06_18_204122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_212525) do
   create_table "trackings", force: :cascade do |t|
     t.bigint "illness_id", null: false
     t.datetime "date"
-    t.integer "mood"
+    t.string "mood"
     t.float "temperature"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
