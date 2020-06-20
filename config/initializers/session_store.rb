7d52e26174f2511aa0621ed40120e56d890c5d71
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-20 18:26:51 -0500

    change configuration of session store to maintain session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_your-app-name', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: '_finalproject-api', domain: 'http://localhost:3000/'
 else
   Rails.application.config.session_store :cookie_store, key: '_finalproject-api'
   end
