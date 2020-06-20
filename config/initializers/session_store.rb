b09dcc03bd837f92358d259b18ffe0078353286a
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-20 18:36:19 -0500

    modify initializer for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_finalproject-api', domain: 'http://localhost:3000/'
+  Rails.application.config.session_store :cookie_store, key: '_finalproject-api', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_finalproject-api'
   end
