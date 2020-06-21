29f843f5bec3a51bd110e335c3cf9f46d40453d3
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-21 10:01:58 -0500

    fix rubocop linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   include ::ActionController::Cookies
 
   skip_before_action :verify_authenticity_token
-  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout! 
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
   def login!
     session[:user_id] = @user.id
   end
