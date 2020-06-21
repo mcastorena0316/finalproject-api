7e7d52e84389c033198af300cd42d774a0c23098
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-21 08:12:03 -0500

    add configuration for deployed site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://illnest.netlify.app'
 
     resource '*',
              headers: :any,
