73643100fafb4f830d7a463cbd2b9d7df484d3b0
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-20 20:01:52 -0500

    trying out the session_store configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_api-finalcapstone', domain: 'illnest-api.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_api_finalcapstone', domain: 'illnest-api.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_api_finalcapstone'
 end
