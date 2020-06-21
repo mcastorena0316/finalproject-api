25f6f36b44950744544684b47a4613a3679d2b06
Melissa Castorena <mcastorena0316@gmail.com>
2020-06-20 20:40:02 -0500

    trying out the session_store configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   include ActionController::Helpers
+  include ::ActionController::Cookies
 
   skip_before_action :verify_authenticity_token
-  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout! 
   def login!
     session[:user_id] = @user.id
   end
